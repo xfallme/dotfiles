4a4ff8ae930365cb177cd50694eba06ca0af0c4e
Maximilian <23641084+xfallme@users.noreply.github.com>
2026-06-10 12:04:56 +0200

    repair .config behaviour

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-stow common
+stow . --target=$HOME --ignore=.stowrc --ignore=common --ignore=macos --ignore=bazzite --ignore=DS_Store --ignore=setup.sh
+stow common --target=$HOME/.config --ignore=DS_Store
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    stow macos --target=$HOME/.config --ignore=DS_Store
+    ln -s ~/.config/ssh/config ~/.ssh/config
+fi
